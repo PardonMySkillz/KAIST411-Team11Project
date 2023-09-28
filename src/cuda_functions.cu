4ac64e4507f54d0a38b43cc33e23943e45ca5c91
PardonMySkillz <hcwong.henry@gmail.com>
2023-09-29 02:31:28 +0900

    batch norm cuda

--- src/cuda_functions.cu
+++ src/cuda_functions.cu
@@ -86,12 +86,55 @@ float* leaky_relu(float* input, int batch_size, int channels, int height, int wi
 
 }
 
-__global__ void _batch_norm(float* input, int batch_size, int channels, int height, int width, float* running_mean, float* running_var, float* weight, float* bias){}
+__global__ void _batch_norm(float* input, float* output, int batch_size, int channels, int height, int width, float* running_mean, float* running_var, float* weight, float* bias){
+
+    uint batch = blockIdx.x;
+    uint channel = blockIdx.y;
+
+    uint row = threadIdx.x;
+    uint col = threadIdx.y;
+
+    uint io_index = batch * channels * height * width + channel * height * width + row * width + col;
+    uint mv_index = batch * channels * width + channel * width + col;
+
+    float e = 1e-5;
+
+    output[io_index] = weight[mv_index] * ((input[io_index] - running_mean[mv_index]) / (running_var[mv_index] + e)) + bias[mv_index];
+
+}
 float* batch_norm(float* input, int batch_size, int channels, int height, int width, float* running_mean, float* running_var, float* weight, float* bias){
     float* output, *device_input, *device_output;
     float *d_running_mean, *d_running_var, *d_weight, *d_bias;
-    unsigned long io_size = batch_size * channels * height * width;
-    unsigned long 
+    unsigned long io_size = batch_size * channels * height * width * sizeof(float);
+    unsigned long mv_size = batch_size * channels * width * sizeof(float);
+
+    cudaMalloc((void**) &device_input, io_size);
+    cudaMalloc((void**) &device_output, io_size);
+    cudaMemcpy(device_input, input, io_size, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**) &d_running_mean, mv_size);
+    cudaMalloc((void**) &d_running_var, mv_size);
+    cudaMalloc((void**) &d_weight, mv_size);
+    cudaMalloc((void**) &d_bias, mv_size);
+    cudaMemcpy(d_running_mean, running_mean, mv_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_running_var, running_var, mv_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_weight, weight, mv_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_bias, bias, mv_size, cudaMemcpyHostToDevice);
+
+    dim3 numBlocks(batch_size, channels);
+    dim3 threadsPerBlock(height, width);
+
+    _batch_norm<<<numBlocks, threadsPerBlock>>>(device_input, device_output, batch_size, channels, height, width, d_running_mean, d_running_var, d_weight, d_bias);
+
+    cudaMemcpy(output, device_output, io_size, cudaMemcpyDeviceToHost);
+    cudaFree(device_input);
+    cudaFree(device_output);
+
+    cudaFree(d_running_mean);
+    cudaFree(d_running_var);
+    cudaFree(d_bias);
+    cudaFree(d_weight);
+    return output;
 
 }
 
