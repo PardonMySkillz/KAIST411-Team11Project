5321307b4547829490efc1107253781f00acad39
PardonMySkillz <hcwong.henry@gmail.com>
2023-10-01 09:33:28 +0900

    Conv C implementation

--- lib/mytorch/conv.py
+++ lib/mytorch/conv.py
@@ -32,17 +32,16 @@ class Conv2d(Functional):
     def c(self, activation, weight, bias, stride):
         
         # TODO
-        batch_size, input_channel, input_height, input_width = activation.shape[0], activation.shape[1], activation.shape[2], activation.shape[3]
-        output_channel, kernel_height, kernel_width = weight.shape[0], weight.shape[2], weight.shape[3]
-        output_height, output_width = (input_height - kernel_height) / stride + 1, (input_width - kernel_width) / stride + 1;
-        inputs_array = np.ascontiguousarray(activation.numpy())
-        weights_array = np.ascontiguousarray(weight.numpy())
-        c_dll.conv2d.restype = POINTER(c_float)
-        output = c_dll.conv2d(batch_size, inputs_array.ctypes.data_as(POINTER(c_float)), c_int32(input_channel), c_int32(input_height), c_int32(input_width),
-                 weights_array.ctypes.data_as(POINTER(c_float)), c_int32(output_channel), c_int32(kernel_height), c_int32(kernel_width), c_int32(stride))
-        arr_output = np.ctypeslib.as_array(output, (batch_size*output_channel*output_height*output_width,1))
-        arr_copied = np.copy(arr_output)
-        return torch.from_numpy(arr_copied.reshape((batch_size,output_channel,output_height,output_width)))
+        activation_pointer = activation["pointer"]
+        weight_pointer = weight["pointer"]
+        bias_pointer = bias["pointer"] if bias is not None else None
+        batch_size, input_channel, input_height, input_width = activation["shape"][0], activation["shape"][1], activation["shape"][2], activation["shape"][3]
+        output_channel, kernel_height, kernel_width = weight["shape"][0], weight["shape"][2], weight["shape"][3]
+        output_height, output_width = (input_height - kernel_height) // stride + 1, (input_width - kernel_width) // stride + 1
+        output = c_dll.conv2d(c_int32(batch_size), activation_pointer, c_int32(input_channel), c_int32(input_height), c_int32(input_width),
+                 weight_pointer, bias_pointer, c_int32(kernel_height), c_int32(kernel_width), 
+                 c_int32(output_channel), c_int32(output_height), c_int32(output_width), c_int32(stride))
+        return (output, (batch_size,output_channel,output_height,output_width))
 
     def cuda(self, activation, weight, bias, stride):
         # TODO
--- src/c_functions.c
+++ src/c_functions.c
@@ -38,7 +38,7 @@ float* leaky_relu(int batch_size, float* input, int channels, int height, int wi
 float* batch_norm(){}
 
 float* conv2d(int batch_size, float* input, int input_channels, int input_height, int input_width,
-              float* weight, int kernel_height, int kernel_width,
+              float* weight, float* bias, int kernel_height, int kernel_width,
               int output_channel, int output_height, int output_width, int stride) {
 	// Compute the output dimensions
 
@@ -57,11 +57,16 @@ float* conv2d(int batch_size, float* input, int input_channels, int input_height
     // Perform the convolution
 	for (int batch = 0; batch < batch_size; batch++){
 	    for (int out_c = 0; out_c < output_channel; out_c++) {
+            float bias_tmp = 0.0;
+            if (bias != NULL){
+                bias_tmp = bias[out_c];
+            }
             for (int out_h = 0; out_h < output_height; out_h++) {
                 for (int out_w = 0; out_w < output_width; out_w++) {
                     int i_h_start = out_h * stride;
                     int i_w_start = out_w * stride;
-
+                    int output_index = batch*output_channel*output_height*output_width + out_c*output_height*output_width + out_h*output_width + out_w;
+                    output[output_index] += bias_tmp;
                     for (int kernel_h = 0; kernel_h < kernel_height; kernel_h++) {
                         for (int kernel_w = 0; kernel_w < kernel_width; kernel_w++) {
                             for (int in_c = 0; in_c < input_channels; in_c++) {
@@ -70,12 +75,12 @@ float* conv2d(int batch_size, float* input, int input_channels, int input_height
                                 if (i_h >= 0 && i_h < input_height && i_w >= 0 && i_w < input_width) {
                                     int input_index = batch*input_channels*input_height*input_width + in_c*input_height*input_width + i_h*input_width + i_w;
                                     int kernel_index = out_c*input_channels*kernel_height*kernel_width + in_c*kernel_height*kernel_width + kernel_h*kernel_width + kernel_w;
-                                    int output_index = batch*output_channel*output_height*output_width + out_c*output_height*output_width + out_h*output_width + out_w;
                                     output[output_index] += input[input_index] * weight[kernel_index];
                                 }
                             }
                         }
                     }
+
                 }
             }
 	    }
