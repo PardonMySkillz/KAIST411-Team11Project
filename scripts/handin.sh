c424518bc8c78765cc72a7106c6a980c7035547d
PardonMySkillz <hcwong.henry@gmail.com>
2023-10-27 22:09:29 +0900

    new file:   11.tar 	new file:   11.tar.gz 	modified:   scripts/handin.sh 	modified:   scripts/log.sh

--- scripts/handin.sh
+++ scripts/handin.sh
@@ -3,4 +3,4 @@ cd $(dirname ${BASH_SOURCE[0]})/..
 
 ./scripts/log.sh
 
-tar -zcvf $TEAM.tar ./lib/model.py ./lib/mytorch/{activation,batchnorm,conv,pad,pooling}.py src Makefile log
+tar -zcvf $TEAM.tar.gz ./lib/model.py ./lib/mytorch/{activation,batchnorm,conv,pad,pooling}.py src Makefile log
